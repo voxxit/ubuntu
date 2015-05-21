31c4138fc2cae1664b1e9e571bd8f4d0fbe4e3e5
Joshua Delsman <j@srv.im>
2015-05-20 20:46:25 -0700

    Get rid of crap we don't need

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -2,37 +2,26 @@
 
 SSH_USER=${SSH_USERNAME:-vagrant}
 
-CLEANUP_PAUSE=${CLEANUP_PAUSE:-0}
-echo "==> Pausing for ${CLEANUP_PAUSE} seconds..."
-sleep ${CLEANUP_PAUSE}
+sleep 0
 
 # Make sure udev does not block our network - http://6.ptmc.org/?p=164
-echo "==> Cleaning up udev rules"
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
-echo "==> Cleaning up leftover dhcp leases"
-# Ubuntu 10.04
-if [ -d "/var/lib/dhcp3" ]; then
-    rm /var/lib/dhcp3/*
-fi
-# Ubuntu 12.04 & 14.04
-if [ -d "/var/lib/dhcp" ]; then
-    rm /var/lib/dhcp/*
-fi 
+# Clean up leftover DHCP leases
+rm /var/lib/dhcp/*
 
 # Add delay to prevent "vagrant reload" from failing
-echo "pre-up sleep 2" >> /etc/network/interfaces
+echo "pre-up sleep 2" | tee -a /etc/network/interfaces
 
-echo "==> Cleaning up tmp"
-rm -rf /tmp/*
+# Clean up temporary directories
+rm -rf /var/lib/apt/lists/* /var/tmp/* /tmp/*
 
-# Cleanup apt cache
+# Clean up apt caches
 apt-get -y autoremove --purge
 apt-get -y clean
 apt-get -y autoclean
 
-echo "==> Installed packages"
 dpkg --get-selections | grep -v deinstall
 
 # Remove Bash history
@@ -41,12 +30,12 @@ rm -f /root/.bash_history
 rm -f /home/${SSH_USER}/.bash_history
 
 # Clean up log files
-find /var/log -type f | while read f; do echo -ne '' > $f; done;
+find /var/log -type f | while read f; do
+  > $f
+done
 
-echo "==> Clearing last login information"
->/var/log/lastlog
->/var/log/wtmp
->/var/log/btmp
+# Clearing last login information
+> /var/log/{lastlog,wtmp,btmp}
 
 # Whiteout root
 count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
@@ -65,5 +54,5 @@ dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
 # Make sure we wait until all the data is written to disk, otherwise
-# Packer might quite too early before the large files are deleted
+# Packer might quit too early before the large files are deleted
 sync
